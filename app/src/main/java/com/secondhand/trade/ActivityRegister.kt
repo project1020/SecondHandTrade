951e6d6be2d5646a8505b717a6c2274283716787
project1020 <andeuloideupeulojegteuyong33@gmail.com>
2023-10-23 22:17:12 +0900

    코드 수정, 기능 추가, 주석 추가

--- app/src/main/java/com/secondhand/trade/ActivityRegister.kt
+++ app/src/main/java/com/secondhand/trade/ActivityRegister.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
@@ -20,7 +21,8 @@ class ActivityRegister : AppCompatActivity() {
     private val inputEmail by lazy { binding.inputEmail }
     private val inputPassword by lazy { binding.inputPassword }
     private val inputPasswordConfirm by lazy { binding.inputPasswordConfirm }
-    private val db = FirebaseFirestore.getInstance()
+    private val db = FirebaseFirestore.getInstance() // 파이어베이스 Firestore 데이터베이스
+    // 뒤로가기 버튼 두 번 클릭 콜백
     private var backPressedTime: Long = 0
     private val callback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -36,14 +38,22 @@ class ActivityRegister : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        this.onBackPressedDispatcher.addCallback(this, callback)
+        this.onBackPressedDispatcher.addCallback(this, callback) // 뒤로가기 버튼 두 번 클릭 콜백 등록
 
+        // edittext 입력시 inputLayout error 삭제
+        binding.editNickname.addTextChangedListener(onTextChanged = { _, _, _, _, -> inputNickname.error = null })
+        binding.editEmail.addTextChangedListener(onTextChanged = { _, _, _, _, -> inputEmail.error = null })
+        binding.editPassword.addTextChangedListener(onTextChanged = { _, _, _, _, -> inputPassword.error = null })
+        binding.editPasswordConfirm.addTextChangedListener(onTextChanged = { _, _, _, _, -> inputPasswordConfirm.error = null })
+
+        // 회원가입 버튼 클릭 이벤트
         binding.btnRegister.setOnClickListener {
             val nickname = binding.editNickname.text.toString()
             val email = binding.editEmail.text.toString()
             val password = binding.editPassword.text.toString()
             val passwordConfirm = binding.editPasswordConfirm.text.toString()
 
+            // edittext 공백 체크
             if (nickname.trim().isEmpty()) inputNickname.error = "닉네임을 입력해 주세요." else inputNickname.error = null
             if (email.trim().isEmpty()) inputEmail.error = "이메일을 입력해 주세요." else inputEmail.error = null
             if (password.trim().isEmpty()) inputPassword.error = "비밀번호를 입력해 주세요." else inputPassword.error = null
@@ -51,54 +61,65 @@ class ActivityRegister : AppCompatActivity() {
             if (email.trim().isNotEmpty() && password.trim().isNotEmpty() && passwordConfirm.trim().isNotEmpty() && (password == passwordConfirm)) doRegister(nickname, email, password)
         }
 
+        // 로그인 텍스트 클릭
         binding.txtLogin.setOnClickListener {
             startActivity(Intent(this, ActivityLogin::class.java)).also { finish() }
         }
     }
 
+    // 회원가입 시도 함수
     private fun doRegister(nickname: String, email: String, password: String) {
+        // isNickNameUnique 함수 호출
         isNicknameUnique(nickname) { isUnique ->
-            if (isUnique) {
+            if (isUnique) { // 닉네임이 존재할 때
                 Firebase.auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        setNickname(Firebase.auth.currentUser?.uid ?: "", nickname, {
+                    if (task.isSuccessful) { // 회원가입 성공
+                        setNickname(Firebase.auth.currentUser?.uid ?: "", nickname, { // setNickname 함수 호출
                             Toast.makeText(this, "회원가입에 성공하였습니다!", Toast.LENGTH_SHORT).show()
                             startActivity(Intent(this, ActivityLogin::class.java)).also { finish() }
-                        }, { })
+                        }, { // 닉네임 설정 실패로 회원가입 실패
+                            Toast.makeText(this, "회원가입에 실패하였습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                        })
                     } else {
                         when {
+                            // 계정이 이미 존재할 때
                             task.exception is FirebaseAuthUserCollisionException -> {
                                 inputEmail.error = "이미 존재하는 계정입니다."
                             }
+                            // 이메일 형식이 올바르지 않을 때
                             task.exception?.message?.contains("The email address is badly formatted") == true -> {
                                 inputEmail.error = "이메일 형식이 올바르지 않습니다."
                             }
+                            // 비밀번호가 6자리 미만일 때
                             task.exception?.message?.contains("The given password is invalid") == true -> {
                                 inputPassword.error = "비밀번호는 6자리 이상이어야 합니다."
                             }
+                            // 그 외
                             else -> {
                                 Toast.makeText(this, "회원가입에 실패하였습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
                             }
                         }
                     }
                 }
-            } else {
+            } else { // 닉네임이 존재하지 않을 때
                 inputNickname.error = "이미 사용 중인 닉네임입니다."
             }
         }
     }
 
+    // 닉네임 중복 확인 함수
     private fun isNicknameUnique(nickname: String, onComplete: (Boolean) -> Unit) {
         db.collection("nicknames").document(nickname).get().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
+            if (task.isSuccessful) { // 닉네임이 존재함
                 val document = task.result
                 onComplete(!document.exists())
-            } else {
+            } else { // 닉네임이 존재하지 않음
                 onComplete(false)
             }
         }
     }
 
+    // 닉네임 설정 함수
     private fun setNickname(userId: String, nickname: String, onSuccess: () -> Unit, onFailure: () -> Unit) {
         db.collection("nicknames").document(nickname).set(mapOf("userId" to userId)).addOnSuccessListener {
             val profileUpdate = UserProfileChangeRequest.Builder()
--- app/src/main/java/com/secondhand/trade/FragmentHome.kt
+++ app/src/main/java/com/secondhand/trade/FragmentHome.kt
@@ -1,17 +1,83 @@
 package com.secondhand.trade
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import com.secondhand.trade.databinding.FragmentHomeBinding
 
 class FragmentHome : Fragment() {
     private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
-
+    private val fabHome by lazy { binding.fabHome }
+    private lateinit var mainActivity: ActivityMain
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as ActivityMain
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // 액션바 추가
+        mainActivity.setSupportActionBar(binding.toolbarHome)
+        // 메뉴 추가 (검색, 필터)
+        mainActivity.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_home_toolbar_item, menu)
+                val searchItem = menu.findItem(R.id.menuSearch)
+                val searchView = searchItem.actionView as SearchView
 
+                searchView.queryHint = "게시글 검색"
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        // 검색 버튼을 눌렀을 때 이벤트 처리
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String): Boolean {
+                        // 검색어 입력 중에 이벤트 처리
+                        return false
+                    }
+                })
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                 when (menuItem.itemId) {
+                     R.id.menuFilter -> {
+                         // 필터 dialog 화면 띄우기
+                         /*
+                              - 가격 조건
+                                ㄴ RangeSlider 사용
+                                   ㄴ 가격 범위 (0원 ~ 1억원)
+                                ㄴ slider 아래 edittext로 수동 입력
+
+                              - 판매된 상품 제외
+                                ㄴ MaterialButtonToggleGroup 사용
+                                ㄴ DB에서 판매 중, 판매 완료 구분
+                         */
+                     }
+                 }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        // recyclerview 스크롤 리스너
+        binding.recyclerHome.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0 && fabHome.isExtended) { // 위로 스크롤
+                    fabHome.shrink() // FAB 축소
+                } else if (dy < 0 && !fabHome.isExtended) { // 아래로 스크롤
+                    fabHome.extend() // FAB 확장
+                }
+            }
+        })
         return binding.root
     }
 }
