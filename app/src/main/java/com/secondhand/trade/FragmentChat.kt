25950ee1f2cd681596764fddaccb624d52bf1e39
seungsang <andeuloideupeulojegteuyong33@gmail.com>
2023-11-12 00:39:46 +0900

    데이터베이스 저장
    자기 uid로 문서 및 하위 컬랙션 만들도록 만듬.
    일단 만드는 중이라 임시로 한 페이지에서 보내기랑 보기 둘 다 할 수 있도록 구현했고(이 부분은 나중에 따로 분리 하겠음)
    메세지 보내고 받을 때 보낸 사람 닉네임도 같이 보내도록 만들었음.
    여기서 디자인+다른 유저에게 보낼 수 있도록 만들면 끝남

--- app/src/main/java/com/secondhand/trade/FragmentChat.kt
+++ app/src/main/java/com/secondhand/trade/FragmentChat.kt
@@ -2,15 +2,34 @@ package com.secondhand.trade
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.secondhand.trade.databinding.FragmentChatBinding
+import com.secondhand.trade.databinding.ItemLayoutBinding
 
+var userId = "userid"
 class FragmentChat : Fragment() {
     private val binding by lazy { FragmentChatBinding.inflate(layoutInflater) }
     private lateinit var mainActivity: ActivityMain
+    private var adapter: MyAdapter? = null
+    private val db: FirebaseFirestore = Firebase.firestore
+    private var receivedMessagesCollectionRef = db.collection("chats").document(userId).collection("receivedmessage")
+    private var snapshotListener: ListenerRegistration? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -18,7 +37,46 @@ class FragmentChat : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-
+        Firebase.auth.currentUser?.let { user ->
+            userId = user.uid
+            receivedMessagesCollectionRef = db.collection("chats").document(userId).collection("receivedmessage")
+        }
+        adapter = MyAdapter(requireContext(), emptyList())
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerview.adapter = adapter  // 어댑터를 리사이클러뷰에 연결
+        updateList()
+        binding.button.setOnClickListener {
+            addItem()
+        }
         return binding.root
+    }
+
+
+    private fun updateList() {
+        receivedMessagesCollectionRef.get().addOnSuccessListener {
+            val items = mutableListOf<Item>()
+            for (doc in it) {
+                items.add(Item(doc))
+            }
+            adapter?.updateList(items)
+        }
+    }
+    private fun addItem() {
+        receivedMessagesCollectionRef = db.collection("chats").document(userId).collection("receivedmessage")
+        Firebase.auth.currentUser?.let { user ->
+            val userId = user.uid
+            db.collection("users").document(userId).get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val name = task.result.getString("nickname")
+                    val messageinput = binding.editTextText.text.toString()
+                    val itemMap = hashMapOf(
+                        "sender" to name,
+                        "text" to messageinput
+                    )
+                    receivedMessagesCollectionRef.add(itemMap).addOnSuccessListener { updateList() }.addOnFailureListener {  }
+                }
+            }
+        }
+
     }
 }
