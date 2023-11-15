024bac12003529379708e2fce82deab9bc1a45cb
seungsang <74907427+seungsaung@users.noreply.github.com>
2023-11-15 20:10:15 +0900

    어 이거 되는지 모르겟네

--- app/src/main/java/com/secondhand/trade/FragmentChat.kt
+++ app/src/main/java/com/secondhand/trade/FragmentChat.kt
@@ -3,25 +3,17 @@ package com.secondhand.trade
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
-import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.secondhand.trade.databinding.FragmentChatBinding
-import com.secondhand.trade.databinding.ItemLayoutBinding
 
 var userId = "userid"
 class FragmentChat : Fragment() {
