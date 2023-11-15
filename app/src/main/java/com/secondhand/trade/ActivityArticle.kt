72c24e2e674ced96f074e5d04dc8c71338594dd1
project1020 <148557775+project1020@users.noreply.github.com>
2023-11-15 20:25:00 +0900

    Update ActivityArticle.kt
    적용되어야 할 data class 세팅

--- app/src/main/java/com/secondhand/trade/ActivityArticle.kt
+++ app/src/main/java/com/secondhand/trade/ActivityArticle.kt
@@ -1,14 +1,21 @@
 package com.secondhand.trade
 
+data class articles (
+    var title: String?= null,
+    var content: String?= null,
+    var price: Int?= null,
+    var imageUrl: String?= null,
+    var isSoldOut: Boolean?= null
+)
+
 class ActivityArticle {
-    // db 연결
+    // db 연결, 및 ui 연동
 
 
-    // db 등록 함수
+    // db에 설정한 값 등록하는 함수
 
 
-    // db update 함수
+    // db에 수정한 값 적용하는 update 함수
 
 
-    // db 전송
 }
