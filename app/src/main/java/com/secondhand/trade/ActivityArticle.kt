b373d2a3fcd5905472a6a2b3ea6db0b12b4a0ee0
project1020 <148557775+project1020@users.noreply.github.com>
2023-11-16 19:37:41 +0900

    1단계
    data class 정의
    addBoard, updateBoard 정의

--- app/src/main/java/com/secondhand/trade/ActivityArticle.kt
+++ app/src/main/java/com/secondhand/trade/ActivityArticle.kt
@@ -1,21 +1,83 @@
 package com.secondhand.trade
 
-data class articles (
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+//import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+data class Articles (
     var title: String?= null,
     var content: String?= null,
     var price: Int?= null,
-    var imageUrl: String?= null,
     var isSoldOut: Boolean?= null
-)
+) {
+    constructor(doc: QueryDocumentSnapshot) : this(
+        title = doc.getString("title"),
+        content = doc.getString("content"),
+        price = doc.getLong("price")?.toInt(),
+        isSoldOut = doc.getBoolean("isSoldOut")
+    )
+}
 
-class ActivityArticle {
+class ActivityArticle : AppCompatActivity() {
+    private val editTitle by lazy { findViewById<EditText>(R.id.editTitle) }            //
+    private val editContent by lazy { findViewById<EditText>(R.id.editContent) }
+    private val editPrice by lazy { findViewById<EditText>(R.id.editPrice) }
+    private val isSoldOut by lazy { findViewById<CheckBox>(R.id.isSoldOut) }
+    //private val imageSelect by lazy { findViewById<ImageView>(R.id.photoImageView) }
+    private val firestore = FirebaseFirestore.getInstance()
+    private val board = firestore.collection("board_test")
     // db 연결, 및 ui 연동
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
 
-    // db에 설정한 값 등록하는 함수
+
+        findViewById<Button>(R.id.btnSubmit).setOnClickListener{
+            addBoard()
+        }
+
+        findViewById<Button>(R.id.btnmodify).setOnClickListener{
+            updateBoard()
+        }
+
+    }
+
+    private fun addBoard() {    // db에 값을 등록하는 함수
+        val title = editTitle.text.toString()
+        val content = editContent.text.toString()
+        val price = editPrice.text.toString().toInt()
+        val isSoldOut = isSoldOut.isChecked
+
+        val itemMap = hashMapOf(
+            "title" to title,
+            "price" to price,
+            "content" to content,
+            "isSoldout" to isSoldOut,
+            //"imageUrl" to imageurl
+        )
+        board.add(itemMap)
+                .addOnSuccessListener { updateBoard() }.addOnFailureListener {  }
+
+    }
 
 
-    // db에 수정한 값 적용하는 update 함수
+    private fun updateBoard() {// db에 이미 있는 값 수정해서 적용하는 update 함수
+        board.get().addOnSuccessListener {
+            val article = mutableListOf<Articles>()
+            for (doc in it) {
+                article.add(Articles(doc))
+            }
+        }
+    }
+
 
 
 }
